0f51264f24aed17bf66b402faff6adae32f84670
Iñigo Vadillo <ivadillo6@gmail.com>
2022-08-25 19:53:17 +0200

    code clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graphics1Component } from './pages/graphics1/graphics1.component';
-import { PagesComponent } from './pages/pages.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { RegisterComponent } from './register/register/register.component';
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    // DashboardComponent,
-    // ProgressComponent,
-    // Graphics1Component,
-    // PagesComponent,
-    // NopagefoundComponent,
-    // HeaderComponent,
-    // SidebarComponent,
-    // BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
